297702f6dd2744b54e14b12c12f0727deeb43d45
cynthia <cynthiakagz@gmail.com>
2023-04-26 22:11:41 +0300

    js function

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 
+fetch('http://localhost:3000/Product')
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+})
+.then(data => {
+  console.log(data); 
+})
+.catch(error => {
+  console.error('Error:', error);
+});
+
   function toggleStarColor(event) {
     const currentColor = event.target.style.color;
   
@@ -35,3 +49,18 @@
       commentInput.value = '';
     });
   });
+
+// document.getElementById("cartIcon").addEventListener("click", function() {
+//     alert("Add this product to cart? ");
+// });
+
+function showAlertOnAllCartIcons() {
+    const cartIcons = document.querySelectorAll(".fas.fa-shopping-cart");
+    const remainingProducts = 5; // Change this to the actual number of products remaining
+  
+    cartIcons.forEach((cartIcon) => {
+      cartIcon.addEventListener("click", () => {
+        alert(`There are ${remainingProducts} products remaining.`);
+      });
+    });
+  }
